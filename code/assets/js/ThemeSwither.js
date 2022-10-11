bc4656ac8b7952512281791e05f36e302c5f2d50
griga <gr_73@inbox.ru>
2022-10-11 04:55:20 +0400

    finish project

--- code/assets/js/ThemeSwither.js
+++ code/assets/js/ThemeSwither.js
@@ -2,26 +2,36 @@ var sune = document.getElementById("themeSwithSune") //sune
 var moon = document.getElementById("themeSwitchMoon") //moon
 var swithcer = document.getElementById("switchTheme") //swith button
 var body = document.getElementById("body")
-var form = document.getElementById("changeThemeForm")
-var form2 = document.getElementById("changeThemeForm2")
-var form3 = document.getElementById("changeThemeForm3")
-var form4 = document.getElementById("changeThemeForm4")
+
+var form = document.getElementsByClassName('form-contactMe');
+
+var circle = document.getElementsByClassName("mySkill-container-skill-circle");
+
+var imageNotion = document.getElementsByClassName("Notion")[0];
+
+var contactUsDark = document.getElementById("resume_container-contacts_dark")
+var contactUsWhite = document.getElementById("resume_container-contacts_white")
+
 
 sune.onclick = function (){
+    contactUsDark.classList.toggle("display-none")
+    contactUsWhite.classList.toggle("display-none")
+
     sune.classList.toggle("WhiteThemeOn")
     body.classList.toggle("darkTheme")
     body.classList.toggle("whiteTheme")
-    form.classList.toggle("darkThemeForm")
-    form.classList.toggle("whiteThemeForm")
 
-    form2.classList.toggle("darkThemeForm")
-    form2.classList.toggle("whiteThemeForm")
+    Array.from(form).forEach( form => {
+        form.classList.toggle("darkThemeForm")
+        form.classList.toggle("whiteThemeForm")
+    });
 
-    form3.classList.toggle("darkThemeForm")
-    form3.classList.toggle("whiteThemeForm")
+    Array.from(circle).forEach(circle => {
+        circle.classList.toggle("mySkill-container-skill-circle-color-dark")
+        circle.classList.toggle("mySkill-container-skill-circle-white")
+    })
 
-    form4.classList.toggle("darkThemeForm")
-    form4.classList.toggle("whiteThemeForm")
+    imageNotion.classList.toggle("notion-changeColor")
 }
 //----------------Dark Theme Switcher----------------//
 
--- code/assets/js/burgerMenu.js
+++ code/assets/js/burgerMenu.js
@@ -1,7 +1,9 @@
 var burgerMenuIco = document.getElementById("burgerMenuIco")
 var burgerMenuNames = document.getElementById("burgerMenuNames")
+var header = document.getElementById("header")
 
 burgerMenuIco.onclick = function (){
     burgerMenuIco.classList.toggle("header_container-burgerMenuICO_active")
     burgerMenuNames.classList.toggle("burgerMenuNamesActive")
+    header.classList.toggle("burgerMenuMobile")
 };
